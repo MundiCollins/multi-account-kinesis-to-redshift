7ef59631eaf6707a6e564a2bee542803cb7041d3
MundiCollins <mundicollins@gmail.com>
2021-09-13 09:46:52 +0300

    Cleaning up

--- environments/s3.tf
+++ environments/s3.tf
@@ -7,19 +7,3 @@ resource "aws_s3_bucket" "user_utm_bucket" {
   bucket = "user-utm"
   acl    = "private"
 }
-
-output "user_event_bucket_arn" {
-  value = aws_s3_bucket.user_event_bucket.arn
-}
-
-output "user_utm_bucket_arn" {
-  value = aws_s3_bucket.user_utm_bucket.arn
-}
-
-output "user_event_bucket_id" {
-  value = aws_s3_bucket.user_event_bucket.id
-}
-
-output "user_utm_bucket_id" {
-  value = aws_s3_bucket.user_utm_bucket.id
-}
