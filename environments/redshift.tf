cd4d98e5ce42cdf326ebc5c10a17d279f278314d
MundiCollins <mundicollins@gmail.com>
2021-09-13 06:26:36 +0300

    Set up redshift

---
+++ environments/redshift.tf
@@ -0,0 +1,11 @@
+resource "aws_redshift_cluster" "events_cluster" {
+  count = terraform.workspace == "warehouse" ? 1 : 0
+
+  cluster_identifier = "events-cluster"
+  database_name      = "events"
+  master_username    = "testuser"
+  master_password    = "Mustbe8characters"
+  node_type          = "dc1.large"
+  cluster_type       = "single-node"
+  skip_final_snapshot = true
+}
