30e1726871719d26fee264a73eb8cb279f8eb5d1
MundiCollins <mundicollins@gmail.com>
2021-09-13 06:44:14 +0300

    Apply lint

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "environments" {
   source = "/environments/"
 
   user_event_bucket_arn = module.s3.user_event_bucket_arn
-  user_event_bucket_id = module.s3.user_event_bucket_id
-  user_utm_bucket_arn = module.s3.user_utm_bucket_arn
-  user_utm_bucket_id = module.s3.user_utm_bucket_id
+  user_event_bucket_id  = module.s3.user_event_bucket_id
+  user_utm_bucket_arn   = module.s3.user_utm_bucket_arn
+  user_utm_bucket_id    = module.s3.user_utm_bucket_id
 }
